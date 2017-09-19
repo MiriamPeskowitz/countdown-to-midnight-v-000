1e3d4bd2943a0cc86a0132526b112b1730111fcb
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-19 17:51:33 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
 #write your code here
-n = 1
+n = 10
 def countdown(n)
-  while n < 10
+  while n > 0
     puts "#{n} SECOND(S)!"
     n -= 1
   end
   puts "HAPPY NEW YEAR!"
 end
 
-n=1
+n=10
 def countdown_with_sleep(n)
-  while n < 10
+  while n > 0
     puts "#{n} SECOND(S)!"
     sleep 1
     n -= 1
