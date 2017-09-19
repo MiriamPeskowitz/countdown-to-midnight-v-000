0346be5033c9d41e3b104f0355ffd1da78e26505
Miriam Peskowitz <miriam5000@gmail.com>
2017-09-19 17:46:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
-
+n = 1
 def countdown
+  while n < 10
+    puts "#{n} seconds"
+  end
+  puts"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  while n < 10
+    puts "#{n} seconds"
+    sleep(1)
+  end
+  puts"HAPPY NEW YEAR!"
 end
